e4b1680fb0aadaa61e75b5aefad941adee285add
FreeJ233 <50964071+FreeJ233@users.noreply.github.com>
2024-09-02 22:53:36 +0800

    Update im.sh

--- sh/im.sh
+++ sh/im.sh
@@ -15,24 +15,6 @@ git clone -b v5-lua --depth 1 --single-branch https://github.com/sbwml/luci-app-
 git clone -b master --depth 1 --single-branch https://github.com/shiyu1314/luci-app-homeproxy package/xd/luci-app-homeproxy
 git clone https://github.com/QiuSimons/luci-app-daed package/dae
 
-cat > ./package/kernel/linux/modules/netsupport.mk<< EOF
-define KernelPackage/xdp-sockets-diag
-  SUBMENU:=$(NETWORK_SUPPORT_MENU)
-  TITLE:=PF_XDP sockets monitoring interface support for ss utility
-  KCONFIG:= \
-	CONFIG_XDP_SOCKETS=y \
-	CONFIG_XDP_SOCKETS_DIAG
-  FILES:=$(LINUX_DIR)/net/xdp/xsk_diag.ko
-  AUTOLOAD:=$(call AutoLoad,31,xsk_diag)
-endef
-
-define KernelPackage/xdp-sockets-diag/description
- Support for PF_XDP sockets monitoring interface used by the ss tool
-endef
-
-$(eval $(call KernelPackage,xdp-sockets-diag))
-EOF
-
 
 ./scripts/feeds update -a
 rm -rf feeds/luci/applications/luci-app-homeproxy
